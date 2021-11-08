2f4a7433647d5a6e490f3b5573365ee3cc77ad18
Bhagyeshpro <bhagyeshpaunikar2001@gmail.com>
2021-11-08 15:16:30 +0530

    All Code Without FULL BUILD

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import API_KEY from "./config";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const API_URL =
+  "https://api.pexels.com/v1/search?query=nature&orientation=portrait&size=small&per_page=20";
+
+const fetchImagesFromPexels = async () => {
+  const data = await fetch(API_URL, {
+    headers: {
+      Authorization: API_KEY,
+    },
+  });
+
+  const results = await data.json();
+};
+
+export default () => {
+  React.useEffect(() => {
+    const fetchImages = async () => {
+      const images = await fetchImagesFromPexels();
+
+      console.log(images);
+    };
+  });
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#161b22",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
